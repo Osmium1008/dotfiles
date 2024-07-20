26dab3143195bf8747842fdf80466a3cb21e0271
Osmium_1008 <osmium1008@gmail.com>
2024-07-20 16:58:41 +0900

    色見本表示やっぱり入れた 使いやすそうなのあったので

---
+++ config/nvim/lua/plugins/highlight_colors.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+---@diagnostic disable-next-line: missing-fields
+return {
+    "brenoprata10/nvim-highlight-colors",
+    event = "VeryLazy",
+    opts = {
+        render = "virtual",
+    },
+}
