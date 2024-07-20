e232eaaf3fdbbd9e8e4f7b0843960e9fb0aa5662
Osmium_1008 <osmium1008@gmail.com>
2024-07-20 12:00:36 +0900

    Git操作をいくらかNeovim上で出来るようにした はず

---
+++ config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+---@diagnostic disable-next-line: missing-fields
+return {
+    "NeogitOrg/neogit",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "sindrets/diffview.nvim",
+        "nvim-telescope/telescope.nvim",
+    },
+    opts = {},
+    cmd = "Neogit",
+}
