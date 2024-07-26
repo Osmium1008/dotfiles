2a5d5ed7d65b14a0633be6903404457696c51993
Osmium_1008 <osmium1008@gmail.com>
2024-07-26 16:55:06 +0900

    matchparen.nvimを入れてみた 便利そう?

---
+++ config/nvim/lua/plugins/matchparen.lua
@@ -0,0 +1,9 @@
+---@type LazySpec
+---@diagnostic disable-next-line: missing-fields
+return {
+    "monkoose/matchparen.nvim",
+    event = "VeryLazy",
+    opts = {
+        on_startup = true,
+    },
+}
